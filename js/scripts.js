0922e3ef071ee786eb6269eace514b09a7af58dc
Amber W <amoothenielson@gmail.com>
2021-12-09 16:31:41 -0800

    rename file to solve problems with button

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function Pizza(size, cheese, pepperoni, pineapple) {
+  this.size = size;
+  this.cheese = cheese;
+  this.pepperoni = pepperoni;
+  this.pineapple = pineapple;
+}
+
+Pizza.prototype.prices = function() {
+  
+}
